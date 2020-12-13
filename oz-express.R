1807bdd0e14ab95b04ceea725ea269b91a63f07f
Adam Shen <46703463+adamshen1@users.noreply.github.com>
2020-12-13 00:53:49 -0500

    Update R file

--- oz-express.R
+++ oz-express.R
@@ -10,8 +10,9 @@ library(lexRankr)
 library(knitr)
 library(kableExtra)
 
-# The Wizard of Oz
+##### The Wizard of Oz #####
 
+##### Data prep #####
 oz_original <- gutenberg_download(55)
 
 chapter_titles <- oz_original %>%
@@ -19,38 +20,38 @@ chapter_titles <- oz_original %>%
   pull(text) %>%
   # There's a space out front in the TOC
   str_remove_all(pattern=" Chapter [IVX]+\\. ") %>%
+  # There's a space in front of Chapter 6's title
+  str_remove_all(pattern="^ ") %>%
   paste0("(", ., ")", collapse="|")
 
 oz_original <- oz_original %>%
   mutate(text=str_remove_all(text, pattern=chapter_titles)) %>%
   slice(-c(1:80))
 
-
-## Do the LexRank
-
-oz_express <- oz_original %>%
+oz_prep <- oz_original %>%
   select(-gutenberg_id) %>%
   mutate(chapter=cumsum(str_detect(text, pattern="^Chapter"))) %>%
   mutate(text=str_remove_all(text, pattern="^Chapter.*$")) %>%
   filter(chapter != 0) %>%
   unnest_tokens(output=sentence, input=text, token="sentences", to_lower=FALSE) %>%
-  # Cloning a dummy chapter column so I can have one inside and one outside after nesting
-  mutate(sentnum=1:n(), chnum=chapter) %>%
+  mutate(sentnum=1:n()) %>%
   mutate(across(chapter, as.factor)) %>%
-  group_by(chnum) %>%
-  nest(documents = c(sentence, sentnum, chapter)) %>%
-  pluck("documents") %>%
-  map_dfr(bind_lexrank, text=sentence, doc_id=chapter, level="sentences",
-          usePageRank=TRUE, continuous=TRUE) %>%
+  group_by(chapter) %>%
+  group_split()
+
+
+##### Do the LexRank #####
+oz_express <- oz_prep %>%
+  map_dfr(
+    bind_lexrank, text=sentence, doc_id=chapter, level="sentences",
+    usePageRank=TRUE, continuous=TRUE
+  ) %>%
   drop_na(lexrank) %>%
   group_by(chapter) %>%
   slice_max(order_by=lexrank, prop=0.2) %>%
-  arrange(sentnum) %>%
-  select(chapter, sentence, lexrank)
+  arrange(sentnum)
 
-
-## Even shorter version
-
+##### Even shorter version #####
 oz_speedrun <- oz_express %>%
   group_by(chapter) %>%
   slice_max(order_by=lexrank, n=1) %>%
